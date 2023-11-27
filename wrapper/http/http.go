af80a9b420bde577081ae95b4c3ffc272848aaed
IhsanArdanto <ihsanardanto@gmail.com>
2023-11-27 14:26:49 +0800

    fix(whitelist): fix behavior for wrapper http

--- wrapper/http/http.go
+++ wrapper/http/http.go
@@ -6,27 +6,29 @@ import (
 	gowhitelist "github.com/ihsanardanto-djoin/go-whitelist"
 )
 
-func IPWhitelistMiddleware(allowedIPs []string, next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		var rw *responseWriter
+func IPWhitelistMiddleware(allowedIPs []string) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			var rw *responseWriter
 
-		clientIP, err := gowhitelist.GetClientIP(r)
-		if err != nil {
-			rw = &responseWriter{w, http.StatusUnauthorized, "Unauthorized, IP Client not detected\n"}
-			return
-		}
+			clientIP, err := gowhitelist.GetClientIP(r)
+			if err != nil {
+				rw = &responseWriter{w, http.StatusUnauthorized, "Unauthorized, IP Client not detected\n"}
+				return
+			}
 
-		clientWList := gowhitelist.IPWhitelist{AllowedIPs: allowedIPs}
+			clientWList := gowhitelist.IPWhitelist{AllowedIPs: allowedIPs}
 
-		// Check if the client's IP is in the whitelist
-		if !clientWList.IsIPAllowed(clientIP) {
-			rw = &responseWriter{w, http.StatusForbidden, "Forbidden\n"}
-			return
-		}
+			// Check if the client's IP is in the whitelist
+			if !clientWList.IsIPAllowed(clientIP) {
+				rw = &responseWriter{w, http.StatusForbidden, "Forbidden\n"}
+				return
+			}
 
-		// Serve the next handler
-		next.ServeHTTP(rw, r)
-	})
+			// Serve the next handler
+			next.ServeHTTP(rw, r)
+		})
+	}
 }
 
 // Custom responseWriter to capture status code
