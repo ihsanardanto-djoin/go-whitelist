4f6f479d1e6b9a19c3c9ba149a790b3c7a286bdb
IhsanArdanto <ihsanardanto@gmail.com>
2023-11-27 14:08:02 +0800

    feat(whitelist): add wrapper for http library

---
+++ wrapper/http/go.mod
@@ -0,0 +1,5 @@
+module github.com/ihsanardanto-djoin/go-whitelist/wrapper/http
+
+go 1.19
+
+require github.com/ihsanardanto-djoin/go-whitelist v0.0.0-20231127045850-6b13297c0395 // indirect
---
+++ wrapper/http/http.go
@@ -0,0 +1,42 @@
+package http
+
+import (
+	"net/http"
+
+	gowhitelist "github.com/ihsanardanto-djoin/go-whitelist"
+)
+
+func IPWhitelistMiddleware(allowedIPs []string, next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var rw *responseWriter
+
+		clientIP, err := gowhitelist.GetClientIP(r)
+		if err != nil {
+			rw = &responseWriter{w, http.StatusUnauthorized, "Unauthorized, IP Client not detected\n"}
+			return
+		}
+
+		clientWList := gowhitelist.IPWhitelist{AllowedIPs: allowedIPs}
+
+		// Check if the client's IP is in the whitelist
+		if !clientWList.IsIPAllowed(clientIP) {
+			rw = &responseWriter{w, http.StatusForbidden, "Forbidden\n"}
+			return
+		}
+
+		// Serve the next handler
+		next.ServeHTTP(rw, r)
+	})
+}
+
+// Custom responseWriter to capture status code
+type responseWriter struct {
+	http.ResponseWriter
+	status  int
+	message string
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.status = code
+	rw.ResponseWriter.WriteHeader(code)
+}
